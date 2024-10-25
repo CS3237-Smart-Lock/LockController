650fccd0c39155aa60dbc16aeabd0b534aa260a3
qrkoh <kohqinruo@gmail.com>
2024-10-25 12:26:33 +0800

    Update LockController.ino

--- LockController/LockController.ino
+++ LockController/LockController.ino
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
+#include <ESP32Servo.h>
 
 // Your Wi-Fi credentials
 const char* ssid = "your_SSID";
@@ -8,6 +9,15 @@ const char* password = "your_PASSWORD";
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
+#define SWITCH 4
+byte locked = LOW;
+int count = 0;
+
+Servo myservo;
+
+int pos = 0;
+int servoPin = 18;
+
 int lockDoor() {
   Serial.println("Locking the door...");
   return 0; 
@@ -15,11 +25,22 @@ int lockDoor() {
 
 int unlockDoor() {
   Serial.println("Unlocking the door...");
+  state = HIGH;
   return 0; 
 }
 
 void setup() {
   Serial.begin(115200);
+
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+
+  myservo.setPeriodHertz(50);
+  myservo.attach(servoPin, 1000, 2000);
+
+  pinMode(SWITCH, INPUT_PULLUP);
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -41,4 +62,15 @@ void setup() {
   server.begin();
 }
 
-void loop() {}
+void loop() {
+  if (digitalRead(SWITCH) == LOW) {
+    state = LOW;
+    Serial.println("Locked");
+    myservo.write(0);
+    delay(100);
+  }
+  if (state) {
+    Serial.println("Unlock");
+    myservo.write(180);
+  }
+}
